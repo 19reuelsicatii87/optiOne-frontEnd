00a35c2c69910681a77c32bcb47a9a4e8090d742
Reuel Sicat II <reuel@truelogic.com.ph>
2021-11-15 07:20:43 +0800

    Banner Changes

--- src/Pages/Components/Banner.js
+++ src/Pages/Components/Banner.js
@@ -8,8 +8,8 @@ function Banner() {
             style={{ backgroundImage: `url(/Images/Veggies.jpg)`, opacity: "0.9" }}>
             <div className="d-flex align-items-center">
                 <div className="d-flex flex-column">
-                    <h1 className="display-5 fw-bold text-light">MALASAKIT OPTI ONE</h1>
-                    <p className="fw-light text-light">Welcome to Malasakit Opti One, Independent Distributor at One Opti Lifestyle International.</p>
+                    <h1 className="display-5 fw-bold text-light">MALASAKIT ONE OPTI</h1>
+                    <p className="fw-light text-light">Welcome to Malasakit One Opti, Independent Distributor at One Opti Lifestyle International.</p>
                     <div className="d-flex justify-content-center">
                         <button type="button" class="btn btn-success w-25">PRODUCTS</button>
                     </div>
